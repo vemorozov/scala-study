4a445a0965fd98d7b60b9b4a1477677ee2b551db
victor.morozov <victor.morozov@mynd.co>
2022-03-31 23:02:38 +0300

    feat: implement all Leest requirements

--- src/main/scala/Leest.scala
+++ src/main/scala/Leest.scala
@@ -1,23 +1,152 @@
 package home
 
-sealed abstract class Leest[+A] {  // List being a trait is abusive to common sense
-  def ::[B >: A](a: B): Leest[B]
+import scala.annotation.tailrec
+
+sealed trait Leest[+A] {
+  val size: Int
 
   def apply(i: Int): A
+
+  def head: A
+
+  def tail: Leest[A]
+
+  def filter(f: A => Boolean): Leest[A]
+
+  def withFilter(f: A => Boolean): WithFilter[A]
+
+  def map[B](f: A => B): Leest[B]
+
+  def flatMap[B](f: A => Leest[B]): Leest[B]
+
+  def ::[B >: A](a: B): Leest[B]
+
+  def :::[B >: A](other: Leest[B]): Leest[B]
+
+  def isEmpty: Boolean
+
+  def nonEmpty: Boolean = !isEmpty
+
+  def reverse: Leest[A]
 }
 
 object Leest {
-  def apply[A](a: A*): Leest[A] = if (a.isEmpty) Nil else Cons(a.head, apply(a.tail: _*)) //what is : _* for?
+  def apply[A](a: A*): Leest[A] =
+    a match {
+      case a if a.isEmpty => Nil
+      case _ => Cons(a.head, Leest(a.tail: _*))
+    }
 }
 
 case object Nil extends Leest[Nothing] {
-  override def ::[B >: Nothing](a: B): Leest[B] = Cons(a, Nil)
+  override val size: Int = 0
+
+  override def filter(f: Nothing => Boolean): Leest[Nothing] = Nil
+
+  override def ::[B >: Nothing](b: B): Leest[B] = Cons(b, Nil)
+
+  override def :::[B](other: Leest[B]): Leest[B] = other
 
   override def apply(i: Int): Nothing = throw new IndexOutOfBoundsException
+
+  override def isEmpty: Boolean = true
+
+  override def reverse: Leest[Nothing] = this
+
+  override def tail: Leest[Nothing] = throw new UnsupportedOperationException("tail of empty leest")
+
+  override def head: Nothing = throw new NoSuchElementException("head of empty leest")
+
+  override def map[B](f: Nothing => B): Leest[B] = Leest[B]()
+
+  override def flatMap[B](f: Nothing => Leest[B]): Leest[B] = Leest[B]()
+
+  override def withFilter(f: Nothing => Boolean): WithFilter[Nothing] = new WithFilter(this, f)
+
+  override def toString = "Leest()"
 }
 
-case class Cons[+A](head: A, tail: Leest[A]) extends Leest[A] {
-  override def ::[B >: A](a: B): Leest[B] = Cons(a, head :: tail)
+final case class Cons[+A](head: A, tail: Leest[A]) extends Leest[A] {
+  override val size: Int = tail.size + 1
 
-  override def apply(i: Int): A = if (i == 0) head else tail(i - 1)
+  override def :::[B >: A](other: Leest[B]): Leest[B] =
+    other match {
+      case Nil => this
+      case _ =>
+        val reverse = other.reverse
+        reverse.tail ::: reverse.head :: this
+    }
+
+  override def ::[B >: A](a: B): Leest[B] = Cons(a, Cons(head, tail))
+
+  override def apply(i: Int): A = i match {
+    case 0 => head
+    case _ => tail(i - 1)
+  }
+
+  override def filter(f: A => Boolean): Leest[A] = head match {
+    case head if f(head) => Cons(head, tail.filter(f))
+    case _ => tail.filter(f)
+  }
+
+  override def isEmpty: Boolean = false
+
+  override def reverse: Leest[A] = {
+    @tailrec
+    def iter(acc: Leest[A], rest: Leest[A]): Leest[A] = {
+      if (rest.isEmpty) acc else rest match {
+        case x Cons xs => iter(Cons(x, acc), xs)
+        case Nil => acc
+      }
+    }
+
+    iter(Nil, this)
+  }
+
+  override def map[B](f: A => B): Leest[B] = Cons(f(head), tail.map(f))
+
+  override def flatMap[B](f: A => Leest[B]): Leest[B] = f(head) ::: tail.flatMap(f)
+
+  override def withFilter(f: A => Boolean): WithFilter[A] = new WithFilter[A](this, f)
+
+  override def toString: String = "Leest(" + head + tailToString
+
+  private def innerToString: String = ", " + head + tailToString
+
+  private def tailToString = tail match {
+    case Nil => ")"
+    case t: Cons[A] => t.innerToString
+  }
+}
+
+class WithFilter[+A](content: Leest[A], filter: A => Boolean) {
+  //todo: Optimize(probably remove recursion)
+  def map[B](mapFunction: A => B): Leest[B] = content match {
+    case Nil => Nil
+    case _ =>
+      val mappedTail = content.tail.withFilter(filter).map(mapFunction)
+      content.head match {
+        case h if filter.apply(h) => mapFunction.apply(h) :: mappedTail
+        case _ => mappedTail
+      }
+  }
+
+  def flatMap[B](mapFunction: A => Leest[B]): Leest[B] = content match {
+    case Nil => Nil
+    case _ =>
+      val mappedTail = content.tail.withFilter(filter).flatMap(mapFunction)
+      content.head match {
+        case h if filter.apply(h) => mapFunction.apply(h) ::: mappedTail
+        case _ => mappedTail
+      }
+  }
+
+  def foreach(f: A => Unit): Unit = content match {
+    case Nil => ()
+    case _ =>
+      if (filter.apply(content.head)) f.apply(content.head)
+      content.tail.withFilter(filter).foreach(f)
+  }
+
+  def withFilter(filter2: A => Boolean) = new WithFilter(content, (a: A) => filter(a) && filter2(a))
 }
--- src/test/scala/Test.scala
+++ src/test/scala/Test.scala
@@ -6,20 +6,112 @@ object Test
 
 
 class CalculatorSpec extends AnyFunSpec {
-  private val leest = Leest(0, 1, 2)
+
+  describe("basic stuff") {
+    it("should create empty leest") {
+      assert(Leest() == Nil)
+    }
+    it("should create actual leest") {
+      assert(Leest(0, 1, 2).isInstanceOf[Leest[Int]])
+      assert(Leest(0, 1, 2).isInstanceOf[Leest[Int]])
+    }
+    it("should be readable") {
+      assertResult("Leest(1, 2, 3)")(Leest(1, 2, 3).toString)
+      assertResult("Leest(1)")(Leest(1).toString)
+      assertResult("Leest()")(Nil.toString)
+    }
+    it("should calculate size") {
+      assert(Leest(0, 1, 2, 3, 4, 5).size == 6)
+      assert(Leest().size == 0)
+      assert(Leest(0).size == 1)
+    }
+  }
 
   describe("retrieving elements") {
+    val leest = Leest(0, 1, 2)
     it("should get by index") {
       assert(leest(1) == 1)
     }
   }
 
   describe("addition") {
+    val leest = Leest(0, 1, 2)
+
     it("should prepend") {
       assert(5 :: leest == Leest(5, 0, 1, 2))
+      assert(5 :: Nil == Leest(5))
+      assert(Nil :: leest == Leest(Nil, 0, 1, 2))
     }
-    it("should append") {
-      assert(leest :: 5 :: Nil == Leest(0, 1, 2, 5))
+
+    it("should concat") {
+      assert(Leest(3, 4) ::: leest == Leest(3, 4, 0, 1, 2))
+      assert(Leest(1) ::: Nil == Leest(1))
+      assert(Nil ::: Leest(1) == Leest(1))
+      assert((Nil ::: Nil).isInstanceOf[Leest[Nothing]])
+    }
+  }
+
+  describe("mapping") {
+    val leest = Leest(0, 1, 2)
+    it("should map") {
+      assert(leest.map(v => v.toString) == Leest("0", "1", "2"))
+    }
+
+    it("should map Nil") {
+      assert(Nil.map(_ => true).isInstanceOf[Leest[Nothing]])
+    }
+
+    it("should flatMap") {
+      assert(leest.flatMap(v => v :: Leest(v + 10)) == Leest(0, 10, 1, 11, 2, 12))
+    }
+
+    it("should flatMap Nil") {
+      assert(Nil.map(_ => Leest(true)) == Leest[Boolean]())
+    }
+  }
+
+  describe("filtering") {
+    val leest = Leest(0, 1, 2)
+    it("should filter") {
+      val goodCars: String => Boolean = {
+        case "BMW" => true
+        case _ => false
+      }
+
+      assert(leest.filter(i => i % 2 == 0) == Leest(0, 2))
+      assert(Leest("Mercedes", "Audi", "BMW").filter(goodCars) == Leest("BMW"))
+    }
+  }
+
+  describe("withFilter") {
+    def self[A]: A => A = i => i
+
+    val wf = Leest(0, 1, 2).withFilter(i => i % 2 == 0)
+
+    it("inits") {
+      assert(wf.isInstanceOf[WithFilter[Int]])
+      assert(Nil.withFilter(_ => true).isInstanceOf[WithFilter[Nothing]])
+    }
+    it("maps") {
+      assertResult(Leest())(Nil.withFilter(_ => true).map(self))
+      assertResult(Leest(0, 2))(wf.map(i => i))
+    }
+    it("flatMaps") {
+      assertResult(Leest(0, 1, 2))(Leest(Leest(0, 1), Leest(2), Leest(3, 4)).withFilter(i => i.head < 3).flatMap(self))
+    }
+    it("allows recursive withFilter") {
+      assertResult(Leest(1))(Leest(0, 1, 2).withFilter(i => i < 2).withFilter(i => i > 0).map(self))
+    }
+    it("works with foreach") {
+      var j = 0
+      Leest(0, 1, 2).withFilter(i => i != 1).foreach(_ => j += 1)
+      assert(j == 2)
+    }
+  }
+
+  describe("reverse") {
+    it("should revert") {
+      assert(Leest(0, 1, 2).reverse == Leest(2, 1, 0))
     }
   }
 }
